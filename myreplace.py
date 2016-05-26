1dc9a480e4061e660228501027f962a1ff205a4e
Laurie Cuffney <Laurie Cuffney>
2016-05-25 20:10:49 -0400

    added replace to find

--- myreplace.py
+++ myreplace.py
@@ -1,14 +1,22 @@
 # practice writing code by recreating the replace method
 
-def myreplace(search_string, target_string):
+def myreplace(search_string, target_string,new_string):
     # finds all instances of target_string in search_string and then replaces
     # them with new_string
-    position = 0
+    start_position = 0
     i=0
-    while position != -1:
-        position = search_string.find(target_string,i)
-        i = position + 1
+    length_old = len(target_string)
+    length_new = len(new_string)
+    while True:
+        start_position = search_string.find(target_string,i)
+        if start_position == -1:
+            break
+        end_position = start_position + length_old
+        search_string = search_string[:start_position] + new_string + search_string[end_position:]
+        i = start_position + length_new #add length_new to avoid error if search_string
+                                        # is a substring of new_string the replacement
+    return search_string
 
 
 text = "hello this is a test string it is a place to find the word is "
-myreplace(text, 'is')
+print myreplace(text, 'hiddy ho','this')
